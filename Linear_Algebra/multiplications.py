812256e90302a65ae6b364232aa340886054873b
abdulla <abdullarustamov365@gmail.com>
2025-12-31 09:06:02 +0500

    matrix_multiplication

---
+++ Linear_Algebra/multiplications.py
@@ -0,0 +1,26 @@
+mat1 = [[1, 2, 3], 
+        [3, 4, 5]]
+
+mat2 = [[5, 6], 
+        [7, 8],
+        [9, 10]]
+
+# Function to multiply two matrices
+def matrix_multiply(A, B):
+  if len(A[0]) != len(B):
+    return "Incompatible matrices"
+  else:
+    result = [[ 0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for _ in result:
+      print(_)
+    for i in range(len(A)):
+      for j in range(len(B[0])):
+        sum = 0
+        for k in range(len(B)):
+          sum += A[i][k] * B[k][j]
+        result[i][j] = sum
+    return result
+# Example usage
+print(matrix_multiply(mat1, mat2))
+# Output: [[46, 52], 
+#          [88, 100]]
